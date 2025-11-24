d516ee409e28dea17a95ed2037a7e4440bd1d367
qkrehwo123 <qkrehwo@gmail.com>
2025-11-24 14:59:00 +0900

    Add files via upload
    Signed-off-by: qkrehwo123 <qkrehwo@gmail.com>

---
+++ 박도재/먀/ai/suggestAI/dto/PlannerRequest.java
@@ -0,0 +1,29 @@
+package com.lms.urbangreen.urbangreenproject.ai.suggestAI.dto;
+
+import lombok.Data;
+
+@Data
+public class PlannerRequest {
+
+    // 1. 거주 형태: residenceType
+    private String residenceType;
+
+    // 2. 공간: space
+    private String space;
+
+    // 3. 일조량: sunlight
+    private String sunlight;
+
+    // 4. 텃밭 목적: purpose
+    private String purpose;
+
+    // 5. 선호 작물: preference
+    private String preference;
+
+    // 6. 예산: budget
+    private String budget;
+
+    // 7. 경험 수준: experienceLevel
+    private String experienceLevel;
+
+}
