fc2072f3993134bee97174f3d31477ff109b972c
k9k8h <rkdrjsgh123@naver.com>
2025-11-25 17:05:30 +0900

    Delete 2025.11.25 AI 마이페이지, 추가 및 강의 상세 페이지 수정 directory
    Signed-off-by: k9k8h <rkdrjsgh123@naver.com>

--- 2025.11.25 AI 마이페이지, 추가 및 강의 상세 페이지 수정/java/suggestAI/dto/AiPlannerSaveRequest.java
+++
@@ -1,9 +0,0 @@
-package com.lms.urbangreen.urbangreenproject.ai.suggestAI.dto;
-
-import lombok.Data;
-
-@Data
-public class AiPlannerSaveRequest {
-    private String plannerName;    // 사용자가 입력한 플래너 이름
-    private String plannerContent; // AI가 생성한 HTML 결과
-}
